c5813ced84b69b02c867b7e96a7b3c7abc308c2d
Mathias <mathischerer@gmail.com>
2025-10-09 15:38:44 -0300

    Create login page

---
+++ views/account/login.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restroomhub - Login</title>
+</head>
+<body>
+    <div class="container">
+        <main class="form-container">
+            <form action="login.php">
+                <h1 class="form-title">Login</h1>
+                    <label>E-mail
+                        <input type="email" name="email" id="email">
+                    </label>
+                    <label>Password
+                        <input type="password" name="password" id="password">
+                    </label>
+            </form>
+        </main>
+    </div>
+</body>
+</html>
