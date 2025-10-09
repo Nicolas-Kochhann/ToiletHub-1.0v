5aba643c144f8a4e171e48ea7bd05637ae19233d
Mathias <mathischerer@gmail.com>
2025-10-09 17:20:07 -0300

    idk

--- views/account/login/index.php
+++ views/account/login/index.php
@@ -7,22 +7,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restroomhub - Log in</title>
-    <link rel="stylesheet" href="../styles/formStyle.css">
+    <title>Bathroomhub - Log in</title>
+    <link rel="stylesheet" href="../../styles/formStyle.css">
 </head>
 <body>
     <div class="container">
         <main class="form-container">
-            <form class="big-form" action="login.php">
+            <form class="big-form" action="login.php" method="POST">
                 <h1 class="form-title">Log in</h1>
                 <label>E-mail
-                    <input type="email" name="email" id="email">
+                    <input type="email" name="email" id="email" required>
                 </label>
                 <label>Password
-                    <input type="password" name="password" id="password">
+                    <input type="password" name="password" id="password" required>
                 </label>
                 <button class="submit">Log in</button>
-                <p>Don't have an account? <strong><a href="./signup.php">Sign up</a></strong></p>
+                <p>Don't have an account? <strong><a href="../signup/">Sign up</a></strong></p>
             </form>
         </main>
     </div>
---
+++ views/list-bathrooms/index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bathroomhub</title>
+    <link rel="stylesheet" href="../../styles/listBathroomStyle.css.css">
+</head>
+<body>
+    <div class="container">
+
+        <header>
+            <img src="" alt="logo">
+            <a href="">
+                Criar Banheiro
+            </a>
+            <div>
+                Perfil
+            </div>
+        </header>
+
+        <main>
+            
+        </main>
+
+    </div>
+</body>
+</html>
