ae1900481981f646a2309634b053269bb6eb9c6a
LuizHenriqueMP <98365070+LuizHenriqueMP@users.noreply.github.com>
2025-10-09 15:46:35 -0300

    Importados os métodos das interfaces

--- src/Models/Bathroom.php
+++ src/Models/Bathroom.php
@@ -2,11 +2,68 @@
 
 namespace Src\Models;
 
-class Bathroom{
+class Bathroom implements ActiveRecord{
     private int $bathroomId;
     private bool $isPaid;
     private int $price;
     private int $lat;
     private int $long;
     private User $owner;
+
+    public function save(): bool{
+        $conn = MySQL::connect();
+        if($this->userId){
+            $sql = "UPDATE bathrooms SET isPaid=:isPaid, price=:price, lat=:lat, long=:long, ownerId=:ownerId WHERE bathroomId=:bathroomId";
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute([
+                'isPaid' => $this->isPaid,
+                'price' => $this->price,
+                'lat' => $this->lat,
+                'long' => $this->long,
+                'ownerId' => $this->owner
+            ]);
+        } else {
+            $sql = "INSERT INTO bathrooms(isPaid, price, lat, long, ownerId) VALUES(:isPaid, :price, :lat, :long, :ownerId)";
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute([
+                'isPaid' => $this->isPaid,
+                'price' => $this->price,
+                'lat' => $this->lat,
+                'long' => $this->long
+            ]);
+        }
+        return $result;
+    }
+
+    public function delete(): bool{
+        $conn = MySQL::connect();
+        $sql = "DELETE FROM bathrooms WHERE bathroomId=:bathroomId";
+        $stmt = $conn->prepare($sql);
+        $result = $stmt->execute(['bathroomId' => $this->bathroomId]);
+        return $result;
+    }
+
+    public static function find($bathroomId): Bathroom{
+        $conn = MySQL::connect();
+        $sql = "SELECT isPaid, price, lat, long, ownerId FROM bathrooms WHERE bathroomId=:bathroomId";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['bathroomId' => $bathroomId]);
+        $bathroom = $stmt->fetch();
+
+        return new Bathroom($bathroom['isPaid'], $bathroom['price'],$bathroom['lat'],$bathroom['long'],$bathroom['ownerId']);
+    }
+
+    public static function listAll(): array{
+        $conn = MySQL::connect();
+        $sql = "SELECT isPaid, price, lat, long, ownerId FROM bathrooms";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        $bathroomss = [];
+        foreach($results as $result){
+            $bathroom = new Bathroom($result['isPaid'], $result['price'],$result['lat'],$result['long'],$result['ownerId'],);
+            $bathrooms[] = $bathroom;
+        }
+        return $users;
+    }
 }
--- src/Models/Review.php
+++ src/Models/Review.php
@@ -2,9 +2,68 @@
 
 namespace Src\Models;
 
-class Review{
+class Review implements ActiveRecord{
     private int $reviewId;
     private string $comment;
     private Bathroom $bathroom;
     private User $user;
+
+    public function save(): bool{
+        $conn = MySQL::connect();
+        if($this->userId){
+            $sql = "UPDATE Users SET username=:username, profilePicture=:profilePicture, email=:email WHERE userId=:userId";
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute([
+                'username' => $this->username,
+                'profilePicture' => $this->profilePicture,
+                'email' => $this->email,
+                'userId' => $this->userId
+            ]);
+        } else {
+            $sql = "INSERT INTO Users(username, email, password) VALUES(:username, :email, :password)";
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute([
+                'username' => $this->username,
+                'email' => $this->email,
+                'password' => password_hash($this->password, PASSWORD_BCRYPT)
+            ]);
+        }
+        return $result;
+    }
+
+    public function delete(): bool{
+        $conn = MySQL::connect();
+        $sql = "DELETE FROM users WHERE userId=:userId";
+        $stmt = $conn->prepare($sql);
+        $result = $stmt->execute(['userId' => $this->userId]);
+        return $result;
+    }
+
+    public static function find($userId): User{
+        $conn = MySQL::connect();
+        $sql = "SELECT username, profilePicture, email FROM users WHERE userId=:userId";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['userId' => $userId]);
+        $user = $stmt->fetch();
+        
+        if(!$user){
+            throw new UserNotFoundException();
+        }
+
+        return new User($user['username'], $user['profilePicture'], $user['email']);
+    }
+
+    public static function listAll(): array{
+        $conn = MySQL::connect();
+        $sql = "SELECT username, profilePicture, email FROM users";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        $users = [];
+        foreach($results as $result){
+            $user = new User($result['username'], $result['profilePicture'], $result['email']);
+            $users[] = $user;
+        }
+        return $users;
+    }
 }
