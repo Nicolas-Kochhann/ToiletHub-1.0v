e9bc87e033098b38f0f37a44c24d8f0d0de97674
LuizHenriqueMP <98365070+LuizHenriqueMP@users.noreply.github.com>
2025-10-13 09:21:28 -0300

    Usando o icon nas páginas de login e signup

--- views/account/login/index.php
+++ views/account/login/index.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bathroomhub - Log in</title>
+    <link rel="icon" href="../../resources/images/shiba_icon.ico">
     <link rel="stylesheet" href="../../styles/loginFormStyle.css">
 </head>
 <body>
