ef5ebaa02e5fbeb7df95dc21ffc7e77fe99d7e3f
Nicolas-Kochhann <kochhann.nicolass.soldi@gmail.com>
2025-10-23 17:48:39 -0300

    muitos homens

--- views/account/edit/index.php
+++ views/account/edit/index.php
@@ -9,6 +9,8 @@ use Src\Models\User;
 
 $error = '';
 
+$user = User::find($_SESSION['userId']);
+
 if(isset($_POST['button'])){
     $u = new User($_POST['email'], $_POST['username']);
     $u->setPassword($_POST['password']);
@@ -33,7 +35,7 @@ if(isset($_POST['button'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToiletHub</title>
     <link rel="icon" href="../resources/images/shiba_icon.ico">
-    <link rel="stylesheet" href="../styles/listStyle.css">
+    <link rel="stylesheet" href="../../styles/listStyle.css">
 </head>
 <body>
     <div class="container">
@@ -51,19 +53,25 @@ if(isset($_POST['button'])){
             ?>
             <div class="profile-container">
                 <a class="link-profile" href="">
-                    <img class="image-profile" src="../resources/images/pfp-default.svg" alt="pfp">
+                    <img class="image-profile" src="../../resources/images/pfp-default.svg" alt="pfp">
                 </a>
             </div>
         </header>
 
         <main>
-            <div class="list-grid">
-                <div class="bathroom-card-container">
-                    <img src="../resources/images/placeholders/brazilian-bathroom.png" alt="" class="bathroom-card-image">
-                    <span class="bathroom-text-container">
-                        <strong>Cag'gannrah (indian toilet)</strong>
-                    </span>
-                </div>
+            <div class="edit-account-form-container">
+                <form class="big-form" action="index.php" method="POST">
+                    <h1 class="form-title">Register</h1>
+                    <div class="error"><?php echo $error; ?></div>
+                    <label for="profilePicture"><img class="edit-account-form-profile-picture" src="<?php echo $user->getProfilePicture() ?? '../../resources/images/pfp-default.svg' ?>" alt="profile picture"></label>
+                    <input type="file" name="profilePicture" id="profilePicture" hidden>
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" required>
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email" required>
+                    <button class="submit" name="button">Save Changes</button>
+                    <p>Already have an account? <strong><a href="../login/">Log in</a></strong></p>
+                </form>
             </div>
         </main>
 
