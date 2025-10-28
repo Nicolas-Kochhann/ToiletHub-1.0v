9b79750a43167308e1b915fd2d6e82d3fa15441e
Mathias Scherer <mathischerer@gmail.com>
2025-10-28 19:12:59 -0300

    tomara que nao cague (xD)

--- views/list-bathrooms/index.php
+++ views/list-bathrooms/index.php
@@ -14,7 +14,7 @@ $bathrooms = Bathroom::listAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bathroomhub</title>
+    <title>ToiletHub</title>
     <link rel="icon" href="../resources/images/shiba_icon.ico">
     <link rel="stylesheet" href="../styles/listStyle.css">
     <link rel="preload" href="../resources/images/toilethub_logo.gif" as="image">
